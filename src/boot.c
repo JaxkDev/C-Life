a1a9fd5be51ce45f643a3cf7f4721d81bb80d877
Jack Honour <gangnam253@gmail.com>
2019-03-06 09:03:44 +0000

    Choose gender

--- src/boot.c
+++ src/boot.c
@@ -5,11 +5,20 @@
 
 int start(){
     logM("Booting software.",0);
+    
+    srand(time(NULL));
+    
     printf("Welcome to C-Life !");
     printf("\nWhile we get things ready take a moment to read the following:\n"); //use printf so it doesnt spam log
     printf("\nTips:\n- Dont always be the smart geeky kid, try something new each time (you may suprise yourself)\n- Tired of being on the run leading a cirminal gang, try something new join the army, enroll in college.\n- Dont be glued to this game do go outside :)\n");
 
     //Get the names:
+    
+    int genderD = rand() % 2;
+    
+    bool gender = genderD == 1 ? 0 : 1;
+    
+    printf("Gender: %s",gender);
 
     char* fName = generateFirstName(0);
     char* lName = generateLastName();
